d245dc969b1e4d9319389ca0efdb2a2dd263878b
Shom770 <shayaanwadkar@gmail.com>
2024-06-02 14:11:34 -0400

    final changes

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ load_dotenv()
 class Booking:
     """Represents a single booking made by a teacher for a study room."""
 
-    AVAILABLE_PERIODS = [
+    AVAILABLE_PERIODS = {
         "Period 1",
         "Period 2",
         "Period 3",
@@ -22,19 +22,20 @@ class Booking:
         "Period 7",
         "Period 8",
         "Lunch"
-    ]
+    }
 
-    def __init__(self, periods_free: list):
-        self.periods_free = periods_free
-        self.periods_unavailable = list(set(self.AVAILABLE_PERIODS).difference(periods_free))
+    def __init__(self, periods_booked: list, booked_by: dict):
+        self.periods_unavailable = sorted(periods_booked)
+        self.periods_free = sorted(list(self.AVAILABLE_PERIODS.difference(periods_booked)))
         self._period_summary = [
-            (period, idx < len(periods_free))
-            for idx, period in enumerate(periods_free + self.periods_unavailable)
+            (period, period in self.periods_free)
+            for period in sorted(self.periods_free + self.periods_unavailable)
         ]
         self.windowed_period_summary = [self._period_summary[i:i+3] for i in range(0, 9, 3)]
         self.availability = int(
             (len(self.AVAILABLE_PERIODS) - len(self.periods_free)) / len(self.AVAILABLE_PERIODS) * 100
         )
+        self.booked_by = booked_by
 
     @classmethod
     def timings_for_period(cls) -> dict[str, tuple[str, str]]:
@@ -54,9 +55,13 @@ class Booking:
 class Room:
     """Represents a room's booking for a certain day, with an assigned ID."""
 
-    def __init__(self, booking_list: Booking, id: int):
+    def __init__(self, name: str, id: int, booking_list: Booking = None):
+        self.name = name
         self.bookings = booking_list
         self.id_ = id
+
+    def with_bookings(self, booking_list: Booking) -> "Room":
+        return Room(self.name, self.id_, booking_list)
 
 
 def send_email(email: str, date: str, room: str, block: str, link: str, subject: str = None, full_text: str = None, **kwargs) -> None:
